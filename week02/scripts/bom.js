2b1da1b35121aaa7d2dd6745a6c0b11b9df1d831
astegosaurus-jay <jjjester117@gmail.com>
2025-05-15 13:35:19 -0600

    week 2 learning activitys

---
+++ week02/scripts/bom.js
@@ -0,0 +1,19 @@
+const input = document.querySelector('#favechap');
+const button = document.querySelector('button');
+const list = document.querySelector('#list');
+
+const li = document.createElement('li');
+const deleteButton = document.createElement('button');
+
+li.textContent = input.value;
+
+deleteButton.textContent = '❌';
+
+li.append(deleteButton);
+list.append(li);
+
+button.addEventListener('click', function() {
+    if (input.value.trim() !== '') { 
+        
+     }    
+})
